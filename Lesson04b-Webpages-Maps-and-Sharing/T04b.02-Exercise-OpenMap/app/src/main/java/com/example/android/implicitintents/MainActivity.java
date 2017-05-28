6ae38990fa8cf5356d6948bc51ba8dcac8eceb44
trblandford4@gmail.com <trblandford4@gmail.com>
2017-05-28 14:32:14 -0400

    Completed Lesson 4

--- Lesson04b-Webpages-Maps-and-Sharing/T04b.02-Exercise-OpenMap/app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ Lesson04b-Webpages-Maps-and-Sharing/T04b.02-Exercise-OpenMap/app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -49,11 +49,16 @@ public class MainActivity extends AppCompatActivity {
      */
     public void onClickOpenAddressButton(View v) {
         // TODO (5) Store an address in a String
+        String address = "6590 James Lee Drive, Hughesville MD 20637";
 
         // TODO (6) Use Uri.Builder with the appropriate scheme and query to form the Uri for the address
+        Uri.Builder builder = new Uri.Builder();
+        builder.scheme("geo")
+                .appendQueryParameter("q", address);
+        Uri locationUri = builder.build();
 
         // TODO (7) Replace the Toast with a call to showMap, passing in the Uri from the previous step
-        Toast.makeText(this, "TODO: Open a map when this button is clicked", Toast.LENGTH_SHORT).show();
+        showMap(locationUri);
     }
 
     /**
@@ -119,6 +124,13 @@ public class MainActivity extends AppCompatActivity {
         // TODO (3) Set the data of the Intent to the Uri passed into this method
 
         // TODO (4) Verify that this Intent can be launched and then call startActivity
+
+    private void showMap(Uri locationUri) {
+        Intent openMapIntent = new Intent(Intent.ACTION_VIEW, locationUri);
+        if(openMapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(openMapIntent);
+        }
+    }
 
 
 }
