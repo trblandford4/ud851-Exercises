2cc695083cd3a27a1384ddc519c154ebe83f5800
trblandford4@gmail.com <trblandford4@gmail.com>
2017-09-13 03:17:25 -0400

    Completed Lesson 5

--- Lesson05b-Smarter-GitHub-Repo-Search/T05b.01-Exercise-SaveResults/app/src/main/java/com/example/android/asynctaskloader/MainActivity.java
+++ Lesson05b-Smarter-GitHub-Repo-Search/T05b.01-Exercise-SaveResults/app/src/main/java/com/example/android/asynctaskloader/MainActivity.java
@@ -17,6 +17,7 @@ package com.example.android.asynctaskloader;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -32,9 +33,8 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Create a static final key to store the query's URL
-
-    // TODO (2) Create a static final key to store the search's raw JSON
+    private static final String GITHUB_QUERY_URL_KEY = "search";
+    private static final String GITHUB_RESULTS_RAW_JSON_KEY = "results";
 
     private EditText mSearchBoxEditText;
 
@@ -59,7 +59,14 @@ public class MainActivity extends AppCompatActivity {
 
         mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
 
-        // TODO (9) If the savedInstanceState bundle is not null, set the text of the URL and search results TextView respectively
+        if(savedInstanceState != null) {
+            if(savedInstanceState.containsKey(GITHUB_QUERY_URL_KEY)) {
+                mUrlDisplayTextView.setText(savedInstanceState.getString(GITHUB_QUERY_URL_KEY));
+            }
+            if(savedInstanceState.containsKey(GITHUB_RESULTS_RAW_JSON_KEY)) {
+                mSearchResultsTextView.setText(savedInstanceState.getString(GITHUB_RESULTS_RAW_JSON_KEY));
+            }
+        }
     }
 
     /**
@@ -151,13 +158,13 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    // TODO (3) Override onSaveInstanceState to persist data across Activity recreation
-    // Do the following steps within onSaveInstanceState
-    // TODO (4) Make sure super.onSaveInstanceState is called before doing anything else
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        String urlTextViewContents = mUrlDisplayTextView.getText().toString();
+        String searchResultsTextViewContents = mSearchResultsTextView.getText().toString();
+        outState.putString(GITHUB_QUERY_URL_KEY, urlTextViewContents);
+        outState.putString(GITHUB_RESULTS_RAW_JSON_KEY, searchResultsTextViewContents);
 
-    // TODO (5) Put the contents of the TextView that contains our URL into a variable
-    // TODO (6) Using the key for the query URL, put the string in the outState Bundle
-
-    // TODO (7) Put the contents of the TextView that contains our raw JSON search results into a variable
-    // TODO (8) Using the key for the raw JSON search results, put the search results into the outState Bundle
+    }
 }
